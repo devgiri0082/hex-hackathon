dbba5bd1119030cbb3bcd5707e5cd6544c64ebfd
Dev Giri <devgiri0082@gmail.com>
2023-07-19 22:31:06 +0545

    add: tailwind and clerk auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  UserButton,
+} from "@clerk/clerk-react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+if (!import.meta.env.VITE_SOME_KEY) {
+  throw new Error("Missing Publishable Key")
 }
 
-export default App
+const clerkPubKey = import.meta.env.VITE_SOME_KEY;
+
+function PublicPage() {
+  return (
+    <>
+      <h1 className="underline">Public page</h1>
+      <a href="/protected">Go to protected page</a>
+    </>
+  );
+}
+
+function ProtectedPage() {
+  return (
+    <>
+      <h1>Protected page</h1>
+      <UserButton />
+    </>
+  );
+}
+
+function ClerkProviderWithRoutes() {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<PublicPage />} />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+        <Route
+          path="/protected"
+          element={
+          <>
+            <SignedIn>
+              <ProtectedPage />
+            </SignedIn>
+             <SignedOut>
+              <RedirectToSignIn />
+           </SignedOut>
+          </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
