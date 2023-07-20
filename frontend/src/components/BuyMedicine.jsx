197d6085c25b60b41fc06646aaa64a2c7a04cdbe
Aayush Shah <58813006+akumaaayush@users.noreply.github.com>
2023-07-20 19:53:08 +0545

    add: SignupPage

--- frontend/src/components/BuyMedicine.jsx
+++ frontend/src/components/BuyMedicine.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { BiSolidCartAlt } from "react-icons/bi";
 import sinex from "../assets/sinex.png";
 import CartButton from "./CartButton";
@@ -23,27 +23,27 @@ function BuyMedicine() {
         }
     }
     return (
-        <section className="flex gap-2 bg-slate-50 border-2">
+        <section className="flex gap-2 border-2 bg-slate-50">
             <div className="p-4">
                 <img src={sinex} className="w-60" />
             </div>
-            <div className="p-6 flex flex-col items-start justify-between">
-                <div className="text-upper flex flex-col gap-1 border-b-myblue border-b-2 pb-2 mb-2">
+            <div className="flex flex-col items-start justify-between p-6">
+                <div className="flex flex-col gap-1 pb-2 mb-2 border-b-2 text-upper border-b-myblue">
                     <div className="text-xl font-medium">Paracetamol</div>
                     <div className="text-myblue">Antibiotics</div>
                     <div>Brand: Afrin No Drip Sinus, Allerest 12 Hour Nasal Spray</div>
                 </div>
-                <div className="text-lower flex flex-col gap-2">
+                <div className="flex flex-col gap-2 text-lower">
                     <div className="flex gap-4">
                         <span className="text-myblue">Rs. 30</span>
-                        <span className="text-slate-500 line-through">Rs. 45</span>
+                        <span className="line-through text-slate-500">Rs. 45</span>
                     </div>
                     <div>
                         <span>Quantity</span>
                         <CartButton quantity={product.quantity} handleCartChange={handleCartChange} />
                     </div>
 
-                    <button className=" bg-myblue text-white px-4 py-2 rounded-md flex gap-2 items-center">
+                    <button className="flex items-center gap-2 px-4 py-2 text-white rounded-md  bg-myblue">
                         <BiSolidCartAlt />
                         <span>Add to cart</span>
                     </button>
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,105 +1,47 @@
+import { useState } from "react";
 import SignUpAsPharmacy from "../components/SignUpAsPharmacy";
+import SignUpAsPatient from "../components/SignUpAsPatient";
 
 const SignUpPage = () => {
   // const handleSubmit = () => {};
+  const [clicked, setClicked] = useState("patient");
 
   return (
     <>
-      <main className="bg-primary-gradient bg-no-repeat bg-cover flex justify-center items-center min-h-[688px] ">
+      <main className="bg-primary-gradient bg-no-repeat bg-cover flex justify-center items-center min-h-[500px] ">
         <div className="flex items-center justify-center h-screen ">
-          <div className="flex flex-col px-8 py-10 border border-gray-900 rounded-lg min-w-[500px] bg-cyan-50">
-            <div className="mt-10 underline text-slate-700">
-              <h1 className="pb-10 text-5xl font-bold text-center">
-                Signup as -
+          <div className="flex flex-col px-8 py-10 border border-gray-900 rounded-lg min-w-[300px] bg-cyan-50">
+            <div className="mt-1 underline text-slate-700">
+              <h1 className="pb-10 text-3xl font-bold text-center">
+                Fill up your additional details below:
               </h1>
             </div>
             <div className="flex justify-center gap-5 p-10">
-            <a
-              href="#_"
-              className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
-            >
-              <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
-              <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                <svg
-                  className="w-5 h-5 text-green-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+              <button
+              
+              style={clicked === "patient" ? {color: "white", backgroundColor: "#0085FF", border: "none"} : {}}
+              
+                className="items-center justify-start py-3 pl-8 pr-8 overflow-hidden font-semibold border-2 rounded text-myblue bg-gray-50 group border-slate-400"
+              >
+                <span
+                  onClick={() => {setClicked('patient') } 
+                }
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                  ></path>
-                </svg>
-              </span>
-              <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                <svg
-                  className="w-5 h-5 text-green-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                  ></path>
-                </svg>
-              </span>
-              <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
-                Patient?
-              </span>
-            </a>
-            <p className="text-xl font-bold">OR,</p>
-            <a
-              href="#_"
-              className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
-            >
-              <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
-              <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                <svg
-                  className="w-5 h-5 text-green-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                  ></path>
-                </svg>
-              </span>
-              <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                <svg
-                  className="w-5 h-5 text-green-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                  ></path>
-                </svg>
-              </span>
-              <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
-                Pharmacy?
-              </span>
-            </a>
+                  Patient
+                </span>
+              </button>
+              <p className="text-xl font-bold">OR</p>
+              <button style = {clicked === "pharmacy" ? {color: "white", backgroundColor: "#0085FF", border: "none"} : {}}
+                className="relative inline-flex items-center justify-start py-3 pl-4 pr-4 overflow-hidden font-semibold transition-all duration-150 ease-in-out border-2 rounded text-myblue bg-gray-50 group border-slate-400"
+                onClick={()=> {setClicked("pharmacy")}}
+              >
+                <span className="w-full" >
+                  Pharmacy
+                </span>
+              </button>
             </div>
             <div>
-              <SignUpAsPharmacy/>
+              {clicked === "patient" ?   <SignUpAsPharmacy /> : <SignUpAsPatient />}
             </div>
           </div>
         </div>
