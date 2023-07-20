ee2bfde8fa3e53dba5f9310857914bfc454b7188
abh1sh3kk <abhishekacharya171@gmail.com>
2023-07-20 10:39:09 +0545

    feat: Add navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import logo from "../assets/logo.svg";
+import { useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    return (
+        <nav className="flex w-screen px-4 py-4 shadow-sm justify-center ">
+            <div className="flex justify-between max-w-[1050px] w-full">
+                <figure>
+                    <img src={logo} />
+                </figure>
+                <ul className="flex gap-4 sm:gap-6">
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About Us</li>
+                    <li onClick={()=>{
+                        console.log("first")
+                        navigate('/medicines')
+                    }}>Medicines</li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
