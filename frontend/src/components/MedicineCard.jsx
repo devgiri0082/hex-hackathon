3dcc28db3cc925f2cb353fa3251dc0ab75cea3f3
abh1sh3kk <abhishekacharya171@gmail.com>
2023-07-20 21:14:14 +0545

    Feat: Make responsie card for medicines

--- frontend/src/components/MedicineCard.jsx
+++ frontend/src/components/MedicineCard.jsx
@@ -3,12 +3,12 @@ import paracetamol from "../assets/paracetamol.png";
 
 function MedicineCard() {
     return (
-        <section className="w-full text-sm flex flex-col justify-between rounded-[10px] hover:shadow-sm cursor-pointer">
+        <section className="w-full max-w-[200px] text-sm flex flex-col justify-between rounded-[10px] hover:shadow-sm cursor-pointer">
             <img src={paracetamol} className="rounded-t-md" />
             <div className="p-4 rounded-b-md bg-white text-slate-600 ">
                 <h3 className="font-medium">Paracetamol 500mg</h3>
                 <div className="flex justify-between">
-                    <span>Rs. 40</span>
+                    <span className="text-myblue">Rs. 40</span>
                     <span className="line-through text-slate-400">Rs. 55</span>
                 </div>
             </div>
--- frontend/src/components/Medicines.jsx
+++ frontend/src/components/Medicines.jsx
@@ -5,7 +5,10 @@ function Medicines() {
     return (
         <main className="bg-slate-200 w-full min-h-full flex-wrap flex flex-col py-6 items-center">
             <div className="max-w-[1050px] w-full flex flex-col justify-start items-start">
-                <div className="flex gap-4">
+                <div className="card-container">
+                    <MedicineCard />
+                    <MedicineCard />
+                    <MedicineCard />
                     <MedicineCard />
                     <MedicineCard />
                     <MedicineCard />
