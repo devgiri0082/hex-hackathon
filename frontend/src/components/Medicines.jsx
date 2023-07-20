91d5d181b03e2908cfd2140a6a5d3e84d53401cb
abh1sh3kk <abhishekacharya171@gmail.com>
2023-07-21 02:20:33 +0545

    Change: Minor CSS changes

--- frontend/src/components/Medicines.jsx
+++ frontend/src/components/Medicines.jsx
@@ -3,7 +3,7 @@ import MedicineCard from "./MedicineCard";
 
 function Medicines() {
     return (
-        <main className="bg-slate-200 w-full min-h-full flex-wrap flex flex-col py-6 items-center">
+        <main className=" w-full min-h-full flex-wrap flex flex-col py-6 items-center">
             <div className="max-w-[1050px] w-full flex flex-col justify-start items-start">
                 <div className="card-container">
                     <MedicineCard />
