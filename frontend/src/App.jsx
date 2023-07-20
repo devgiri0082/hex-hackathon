21e5c69220ef451fc049b04c4901b62676d311bc
Aayush Shah <58813006+akumaaayush@users.noreply.github.com>
2023-07-20 17:45:19 +0545

    add: DetailsForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,54 @@
 import {
-    ClerkProvider,
-    SignedIn,
-    SignedOut,
-    RedirectToSignIn,
-    SignIn,
-    SignUp,
-    UserButton,
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  UserButton,
 } from "@clerk/clerk-react";
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import FetchButton from "./components/Button";
 import Homepage from "./pages/Homepage";
 import MedicineList from "./pages/MedicineList";
+<<<<<<< HEAD
 import MedicineInfo from "./pages/MedicineInfo";
 import AdminPage from './pages/AdminPage'
+=======
+import AdminPage from "./pages/AdminPage";
+import SignUpPage from "./pages/SignupPage";
+>>>>>>> 33a17e7 (add: DetailsForm)
 
 
 if (!import.meta.env.VITE_SOME_KEY) {
-    throw new Error("Missing Publishable Key");
+  throw new Error("Missing Publishable Key");
 }
 
 const clerkPubKey = import.meta.env.VITE_SOME_KEY;
 
 function PublicPage() {
-    return (
-        <>
-            <h1 className="underline">Public page</h1>
-            <a href="/protected">Go to protected page</a>
-        </>
-    );
+  return (
+    <>
+      <h1 className="underline">Public page</h1>
+      <a href="/protected">Go to protected page</a>
+    </>
+  );
 }
 
 function ProtectedPage() {
-    return (
-        <>
-            <h1>Protected page</h1>
-            <UserButton />
-            <FetchButton />
-        </>
-    );
+  return (
+    <>
+      <h1>Protected page</h1>
+      <UserButton />
+      <FetchButton />
+    </>
+  );
 }
 
 function ClerkProviderWithRoutes() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+<<<<<<< HEAD
     return (
         <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
             <Routes>
@@ -68,14 +74,47 @@ function ClerkProviderWithRoutes() {
             </Routes>
         </ClerkProvider>
     );
+=======
+  return (
+    <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/medicines" element={<MedicineList />} />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/protected"
+          element={
+            <>
+              <SignedIn>
+                <ProtectedPage />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  );
+>>>>>>> 33a17e7 (add: DetailsForm)
 }
 
 function App() {
-    return (
-        <BrowserRouter>
-            <ClerkProviderWithRoutes />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,111 @@
+import SignUpAsPharmacy from "../components/SignUpAsPharmacy";
+
+const SignUpPage = () => {
+  // const handleSubmit = () => {};
+
+  return (
+    <>
+      <main className="bg-primary-gradient bg-no-repeat bg-cover flex justify-center items-center min-h-[688px] ">
+        <div className="flex items-center justify-center h-screen ">
+          <div className="flex flex-col px-8 py-10 border border-gray-900 rounded-lg min-w-[500px] bg-cyan-50">
+            <div className="mt-10 underline text-slate-700">
+              <h1 className="pb-10 text-5xl font-bold text-center">
+                Signup as -
+              </h1>
+            </div>
+            <div className="flex justify-center gap-5 p-10">
+            <a
+              href="#_"
+              className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+            >
+              <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
+              <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+                <svg
+                  className="w-5 h-5 text-green-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                <svg
+                  className="w-5 h-5 text-green-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                Patient?
+              </span>
+            </a>
+            <p className="text-xl font-bold">OR,</p>
+            <a
+              href="#_"
+              className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+            >
+              <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
+              <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+                <svg
+                  className="w-5 h-5 text-green-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                <svg
+                  className="w-5 h-5 text-green-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                Pharmacy?
+              </span>
+            </a>
+            </div>
+            <div>
+              <SignUpAsPharmacy/>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default SignUpPage;
