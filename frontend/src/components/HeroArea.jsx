ef379d3d171b2c8bd76655d678a410b54c23d85e
Aayush Shah <58813006+akumaaayush@users.noreply.github.com>
2023-07-20 13:15:36 +0545

    add: AdminPage

--- frontend/src/components/HeroArea.jsx
+++ frontend/src/components/HeroArea.jsx
@@ -5,19 +5,15 @@ function HeroArea() {
     return (
         <main className="bg-primary-gradient bg-no-repeat bg-cover flex justify-center items-center min-h-[680px] w-full px-4 sm:px-8 lg:px-0">
             <div className="flex max-w-[1050px] justify-center items-center px-4 sm:px-0 gap-8">
-                <section className="flex flex-col gap-4 items-start">
-                    <article className="text-slate-600 text-5xl max-w-[350px] font-medium leading-normal">
-                        Your Medicine On The Go!
-                    </article>
+                <section className="flex flex-col items-start gap-4">
+                    <article className="text-slate-600 text-5xl max-w-[350px] font-medium">Your Medicine On The Go!</article>
                     <article className="text-slate-500 max-w-[400px]">
                         Experience the convenience of modern medicine delivery with MediFast. Join
                         us today and witness the difference that fast, reliable, and easy-to-use
                         service can make in your healthcare journey. Whether you're tech-savvy or
                         not, you'll find navigating our platform a breeze.
                     </article>
-                    <button className="text-lg bg-myblue text-white px-4 py-2 rounded-md">
-                        Get Started
-                    </button>
+                    <button className="px-4 py-2 text-lg text-white rounded-md bg-myblue">Get Started</button>
                 </section>
                 <section>
                     <figure className="">
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,21 +4,19 @@ import { useNavigate } from "react-router-dom";
 const Navbar = () => {
     const navigate = useNavigate();
     return (
-        <nav className="flex w-screen px-4 py-4 shadow-sm justify-center text-slate-500 ">
+        <nav className="flex w-screen px-4 py-4 shadow-sm justify-center ">
             <div className="flex justify-between max-w-[1050px] w-full">
                 <figure>
                     <img src={logo} />
                 </figure>
                 <ul className="flex gap-4 sm:gap-6">
-                    <li className="border-b-2 border-transparent hover:border-myblue px-1 cursor-pointer">
-                        Home
-                    </li>
-                    <li className="border-b-2 border-transparent hover:border-myblue px-1 cursor-pointer">
-                        Medicines
-                    </li>
-                    <li className="bg-myblue text-white py-1 px-3 rounded-md hover:opacity-70 cursor-pointer">
-                    Sign In
-                    </li>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About Us</li>
+                    <li onClick={()=>{
+                        console.log("first")
+                        navigate('/medicines')
+                    }}>Medicines</li>
                 </ul>
             </div>
         </nav>
