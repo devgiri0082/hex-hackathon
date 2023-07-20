1f92098f14e9d9f2bedfd1cda758d6de23f346cc
abh1sh3kk <abhishekacharya171@gmail.com>
2023-07-20 13:19:21 +0545

    Fix: Minor css fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 
 import logo from "../assets/logo.svg";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const navigate = useNavigate();
     return (
-        <nav className="flex w-screen px-4 py-4 shadow-sm justify-center ">
+        <nav className="flex w-screen px-4 py-4 shadow-sm justify-center text-slate-500 ">
             <div className="flex justify-between max-w-[1050px] w-full">
                 <figure>
                     <img src={logo} />
                 </figure>
                 <ul className="flex gap-4 sm:gap-6">
-                    <li>Home</li>
-                    <li>Contact</li>
-                    <li>About Us</li>
-                    <li onClick={()=>{
-                        console.log("first")
-                        navigate('/medicines')
-                    }}>Medicines</li>
+                    <li className="border-b-2 border-transparent hover:border-myblue px-1 cursor-pointer">
+                        Home
+                    </li>
+                    <li className="border-b-2 border-transparent hover:border-myblue px-1 cursor-pointer">
+                        Medicines
+                    </li>
+                    <li className="bg-myblue text-white py-1 px-3 rounded-md hover:opacity-70 cursor-pointer">
+                    Sign In
+                    </li>
                 </ul>
             </div>
         </nav>
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -7,21 +7,21 @@ function SearchBar() {
         setSearchText(e.target.value);
     };
     return (
-        <div className="flex gap-4">
-            <div className="search-area relative flex justify-center items-center gap-2 ">
-                <GoSearch className="relative left-8" />
-                <input
-                    placeholder="Search..."
-                    value={searchText}
-                    onChange={handleSearch}
-                    className="ps-8 h-10 w-[8rem] sm:w-auto text-sm rounded-md outline-transparent border-none focus:outline-myblue focus:outline-[1px]"
-                />
+        <div className="flex gap-4 w-screen text-slate-500 justify-center items-center">
+            <div className="min-w-[1050px]">
+                <div className="flex gap-4 relative right-8">
+                    <div className="search-area relative flex justify-center items-center gap-2 ">
+                        <GoSearch className="relative left-8" />
+                        <input
+                            placeholder="Search..."
+                            value={searchText}
+                            onChange={handleSearch}
+                            className="ps-8 h-10 w-[8rem] sm:w-auto text-sm rounded-md outline-transparent border-none focus:outline-myblue bg-blue-50 focus:outline-[1px]"
+                        />
+                    </div>
+                    <button className="bg-myblue text-white px-4 py-2 rounded-md">Search</button>
+                </div>
             </div>
-            {/* <div className="flex gap-2 items-center bg-slate-400 rounded-sm">
-                <GoSearch className="text-white"/>
-                <input className="" placeholder="Search here..." />
-            </div> */}
-            <button className="bg-myblue text-white px-4 py-2 rounded-md">Search</button>
         </div>
     );
 }
